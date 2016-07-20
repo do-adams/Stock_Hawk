309696834b453f2232f2ff07b7b26b5b240553d4
Damian Adams <reikjavic@live.com>
2016-07-20 14:54:59 -0400

    feat: Adds empty Widget components for building new Widget functionality for the application.

---
+++ app/src/main/java/com/damian/android/stockhawk/widget/StocksWidgetProvider.java
@@ -0,0 +1,13 @@
+package com.damian.android.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class StocksWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
